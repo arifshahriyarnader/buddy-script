60c30c2bd4b50338356e87572307c29d14a5a9d4
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-11-22 13:06:53 +0600

    add MongoDB enviroment variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import { appConfig } from './config';
+import connectDB from './db';
 
 const app: Application = express();
 app.use(bodyParser.json());
 app.use(express.json());
 
+connectDB()
 
 app.listen(appConfig.PORT, () => {
     console.log(`Server is running on port ${appConfig.PORT}`);
