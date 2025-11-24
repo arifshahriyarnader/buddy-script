c2de962de60485308d5849f7986a19cff30f12af
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-11-25 01:18:56 +0600

    add cors and setup vite-base-url

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import { appConfig } from "./config";
 import connectDB from "./db";
 import apiRoutes from "./routes/api";
 
 const app: Application = express();
+app.use(cors({ origin: appConfig.ALLOWED_ORIGIN }));
 app.use(bodyParser.json());
 app.use(express.json());
 
