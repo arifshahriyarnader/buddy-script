ccc68fe1301a64539ed6e15ed2f90fec74960b98
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-11-25 13:22:45 +0600

    move registration form logic into custom hook

--- client/src/components/auth/RegistrationForm.tsx
+++ client/src/components/auth/RegistrationForm.tsx
@@ -1,73 +1,11 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
-import type { RegistrationFormData } from "../../auth/authTypes";
-import { useState } from "react";
-import { authServices } from "../../auth";
+import { useRegistrationForm } from "../../hooks";
 
 const RegistrationForm = () => {
-  const [formData, setFormData] = useState<RegistrationFormData>({
-    firstname: "",
-    lastname: "",
-    email: "",
-    password: "",
-    repeatPassword: "",
-  });
-  const [honeypot, setHoneypot] = useState<string>("");
-  const navigate = useNavigate();
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.name === "honeypot") {
-      setHoneypot(e.target.value);
-      return;
-    } else {
-      setFormData({
-        ...formData,
-        [e.target.name]: e.target.value,
-      });
-    }
-  };
-  const isValidEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-  const isValidPassword = (password: string) => {
-    return password.length >= 6;
-  };
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (honeypot) {
-      console.log("Bot detected!");
-      return;
-    }
-    if (!isValidEmail(formData.email)) {
-      alert("Please enter a valid email address.");
-      return;
-    }
-    if (!isValidPassword(formData.password)) {
-      alert("Password must be at least 6 characters long.");
-      return;
-    }
-    if (formData.password !== formData.repeatPassword) {
-      alert("Passwords do not match.");
-      return;
-    }
-    const payload = {
-      firstname: formData.firstname,
-      lastname: formData.lastname,
-      email: formData.email,
-      password: formData.password,
-      repeatPassword: formData.repeatPassword,
-    };
-    try {
-      await authServices.registration(payload);
-      alert("Registration successful! Now you can log in.");
-      setTimeout(() => {
-        navigate("/login");
-      });
-    } catch (error) {
-      console.error("Registration failed:", error);
-      alert("Registration failed. Please try again.");
-    }
-  };
+  const { formData, honeypot, handleChange, handleSubmit } =
+    useRegistrationForm();
+
   return (
     <div>
       <div className="absolute top-0 left-0 w-40 h-40 bg-purple-300 rounded-full opacity-30 blur-2xl"></div>
---
+++ client/src/hooks/useRegistrationForm.ts
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { authServices } from "../auth";
+import { useNavigate } from "react-router-dom";
+import type { RegistrationFormData } from "../auth/authTypes";
+
+export const useRegistrationForm = () => {
+  const [formData, setFormData] = useState<RegistrationFormData>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+  const [honeypot, setHoneypot] = useState<string>("");
+  const navigate = useNavigate();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "honeypot") {
+      setHoneypot(e.target.value);
+      return;
+    } else {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  const isValidPassword = (password: string) => {
+    return password.length >= 6;
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (honeypot) {
+      console.log("Bot detected!");
+      return;
+    }
+    if (!isValidEmail(formData.email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+    if (!isValidPassword(formData.password)) {
+      alert("Password must be at least 6 characters long.");
+      return;
+    }
+    if (formData.password !== formData.repeatPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+    const payload = {
+      firstname: formData.firstname,
+      lastname: formData.lastname,
+      email: formData.email,
+      password: formData.password,
+      repeatPassword: formData.repeatPassword,
+    };
+    try {
+      await authServices.registration(payload);
+      alert("Registration successful! Now you can log in.");
+      setTimeout(() => {
+        navigate("/login");
+      });
+    } catch (error) {
+      console.error("Registration failed:", error);
+      alert("Registration failed. Please try again.");
+    }
+  };
+  return { formData, honeypot, handleChange, handleSubmit };
+};
